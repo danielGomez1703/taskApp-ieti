09da8910482f8c997fc88cf18cce0d395df98ba0
Daniel Gomez <danipipe1703@gmail.com>
2020-09-02 21:44:04 -0500

    entrega de laboratorio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom'
+
+import Login from "./components/Login";
+import logo from './logo.svg';
+import TaskApp from './components/TaskApp';
+
+
+localStorage.setItem("mail", "prueba@gmail.com");
+localStorage.setItem("password", "prueba1234");
+
+
+class App extends Component {
+    
+    constructor(props) {
+    
+        super(props);
+        this.state = { isLogged: false };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    render() {
+
+        const LoginView = () => (
+            <Login handleClick={this.handleClick} />
+        );
+
+        const TodoAppView = () => (
+            <TaskApp />
+        );
+
+        const view = this.state.isLogged ? TodoAppView : LoginView
+        
+        return (
+            <div className="App">
+
+            <Router>
+              
+
+                    <br />
+                    <br />
+                       
+                        <Route exact path="/" component={view} />
+                    
+                 
+                     </Router>
+            </div>
+        );
+    
+    }
+
+    handleClick(e) {
+        this.setState({ isLogged: true })
+    }
+
+
 }
 
 export default App;
